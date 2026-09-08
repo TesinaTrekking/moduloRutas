21b7424cce1360383795f678b7f5f34c6782a601
benjaminoyarzun <benjaminoyarzuninfo@gmail.com>
2026-09-08 09:53:49 -0300

    arreglado alto de scene

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("rutas"), 640, 480);
+        scene = new Scene(loadFXML("rutas"), 640, 680);
         stage.setScene(scene);
         stage.show();
     }
